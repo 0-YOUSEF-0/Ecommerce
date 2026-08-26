70cf9a44add7b42a4405ee895d439e8f79fceb67
Yousef Samaro <yousefrsamaro5@gmail.com>
2026-08-27 01:39:57 +0300

    changing the Layout of User and creating the home controller for  User

---
+++ Ecommerce3/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce3.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
