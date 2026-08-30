9a031159188f087350509cd2d35e5880418e0fb3
Yousef Samaro <yousefrsamaro5@gmail.com>
2026-08-30 18:35:26 +0300

    add the class to db as table named categories

--- Ecommerce3/Data/ApplicationDbContext.cs
+++ Ecommerce3/Data/ApplicationDbContext.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ecommerce3.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce3.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Category> categories { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            
-            optionsBuilder.UseSqlServer("Data Source=.;datebase=Ecommerce Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30")
+
+            optionsBuilder.UseSqlServer("Data Source=.;Ecommerce ;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
         }
     }
 }
